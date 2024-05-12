e4ffe449c682207b8404bebaf61c5ec0b4e78d7d
Katz00Wonder <artem.ushenin.04@mail.ru>
2024-05-12 18:50:55 +0300

    Рабочая 1 лабораторная работа, с ограничениями(адекватно принимает результат до 255 числа, то есть лучше задавать маленькие значения)

--- Assembly/laboratory_1/laboratory_1.asm
+++ Assembly/laboratory_1/laboratory_1.asm
@@ -1,82 +1,31 @@
-; Объявление переменных A, B, C и X
-section .data
-    prompt_a: db "Enter value for A: ", 0xa
-    prompt_b: db "Enter value for B: ", 0xa
-    prompt_c: db "Enter value for C: ", 0xa
-    newline: db 0xa
+; Х= 3 (А - 4В ) + С / 4
 
-section .bss
-    a: resd 1
-    b: resd 1
-    c: resd 1
-    result: resd 1
+global _start ; объявление метки _start - точки входа в программу
+section .data ; секция данных
+    A dq 30 ; А
+    B dq 6 ; В
+    C dq 20 ; С
 
-section .text
-    global _start
+section .text ; объявление секции кода
 _start:
-    ; Вывод приглашения для ввода значения A
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, prompt_a
-    mov edx, 12
-    int 80h
+    ; Вычисление А - 4В
 
-    ; Чтение значения A
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, a
-    mov edx, 4
-    int 80h
+   mov rax, [C] ; С
+   mov rcx, 4 ; 4
+   idiv rcx ; С / 4
 
-    ; Вывод приглашения для ввода значения B
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, prompt_b
-    mov edx, 12
-    int 80h
+   mov rcx, rax ; резульат С / 4
 
-    ; Чтение значения B
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, b
-    mov edx, 4
-    int 80h
+   mov rax, [A] ; А
+   mov rbx, [B] ; В
 
-    ; Вывод приглашения для ввода значения C
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, prompt_c
-    mov edx, 12
-    int 80h
+   imul rbx, 4 ; 3 * 4
+   sub rax, rbx ; А - 4В
+   imul rax, 3; 3 * (А - 4В)
 
-    ; Чтение значения C
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, c
-    mov edx, 4
-    int 80h
+   add rax, rcx ; 3 * (А - 4В) + С / 4
 
-    ; Загрузка адреса переменной X в регистр
-    lea ecx, [result]
+   mov rdi, rax ; резульат
 
-    ; Вычисление результата
-    mov eax, [a]
-    mov ebx, [b]
-    mov ecx, [c]
-    imul ebx, 4
-    sub eax, ebx
-    imul eax, 3
-    cdq
-    idiv ecx
-    mov [ecx], eax
-
-    ; Вывод результата
-    mov eax, 4
-    mov ebx, 1
-    mov edx, 4
-    int 80h
-
-    ; Завершение программы
-    mov eax, 1
-    mov ebx, 0
-    int 80h
+   mov rax, 60 ; системный вызов для завершения программы
+   syscall ; вызов системного прерывания
